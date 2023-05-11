b29a152634becb31265037385c566a1c8a8fd9ef
Jacob Götz <jackie35er@gmail.com>
2023-05-11 20:40:58 +0200

    Initial commit

---
+++ src/test/kotlin/org/jackie35er/socail/SocailApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jackie35er.socail
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocailApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
